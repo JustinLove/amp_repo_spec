d3535008675b8b69aa8dbd7d600be4b191188000
Justin Love <git@JustinLove.name>
2011-01-10 15:31:33 -0600

    localrepo.config

--- lib/amp_repo_spec/local_repository.rb
+++ lib/amp_repo_spec/local_repository.rb
@@ -1,6 +1,8 @@
+require 'amp_repo_spec/local_repository/config'
 require 'amp_repo_spec/local_repository/staging_area'
 
 shared_examples_for 'local repository' do
   it {should be_kind_of(Enumerable)}
+  it_should_behave_like 'local repository config'
   it_should_behave_like 'local repository staging area'
 end
---
+++ lib/amp_repo_spec/local_repository/config.rb
@@ -0,0 +1,13 @@
+shared_examples_for 'local repository config' do
+  it {should respond_to :config}
+
+  describe 'without no config specified' do
+    its(:config) {should be_nil}
+  end
+
+  describe 'with a config specified' do
+    let :options do {:ye => :ha} end
+    subject {described_class.new("", nil, options)}
+    its(:config) {should == options}
+  end
+end
