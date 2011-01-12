f05fcba0063e3b553a8d706b3d57686b479479fa
Justin Love <git@JustinLove.name>
2011-01-12 11:09:43 -0600

    shared examples with one loop

--- lib/amp_repo_spec/local_repository.rb
+++ lib/amp_repo_spec/local_repository.rb
@@ -1,18 +1,14 @@
-local_repository_methods = []
-
-Dir.glob(File.join(File.dirname(__FILE__), 'local_repository/*.rb')).each do |file|
-  require file
-  local_repository_methods << File.basename(file, '.rb') 
-end
-
 require 'amp_repo_spec/amp_spec_helper'
 
 shared_examples_for 'local repository' do
   include AmpRepoSpec::Helper
   
   subject {described_class.new(tempdir)}
+
   it {should be_kind_of(Enumerable)}
-  local_repository_methods.each do |meth|
-    it_should_behave_like 'LocalRepository#' + meth
+
+  Dir.glob(File.join(File.dirname(__FILE__), 'local_repository/*.rb')).each do |file|
+    require file
+    it_should_behave_like 'LocalRepository#' + File.basename(file, '.rb')
   end
 end
