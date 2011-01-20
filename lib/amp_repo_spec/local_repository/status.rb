1848d647eee000001cccae239692836457e3fd05
Justin Love <git@JustinLove.name>
2011-01-20 16:40:14 -0600

    placeholder for status

---
+++ lib/amp_repo_spec/local_repository/status.rb
@@ -0,0 +1,19 @@
+shared_examples_for 'local_repository#status' do
+  it {should respond_to :status}
+
+  describe "working to new repo" do
+    pending
+  end
+
+  describe "working to tip" do
+    pending
+  end
+
+  describe "working to revision" do
+    pending
+  end
+
+  describe "revision to revision" do
+    pending
+  end
+end
