c1352521eac107d67a7b12e90476005c0dc84467
Justin Love <git@JustinLove.name>
2011-01-23 20:44:22 -0600

    mark push/pull pending

---
+++ lib/amp_repo_spec/local_repository/pull.rb
@@ -0,0 +1,4 @@
+shared_examples_for 'local_repository#pull' do
+  it {should respond_to :pull}
+  pending "Why don't we get local working first"
+end
---
+++ lib/amp_repo_spec/local_repository/push.rb
@@ -0,0 +1,4 @@
+shared_examples_for 'local_repository#push' do
+  it {should respond_to :push}
+  pending "Why don't we get local working first"
+end
