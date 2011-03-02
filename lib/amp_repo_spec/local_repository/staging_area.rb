e22ed0f64fc2b98d890636ade89ed2f608e07b18
Justin Love <git@JustinLove.name>
2011-03-02 15:01:49 -0600

    refactor staging area quack

--- lib/amp_repo_spec/local_repository/staging_area.rb
+++ lib/amp_repo_spec/local_repository/staging_area.rb
@@ -3,5 +3,5 @@ require 'amp_repo_spec/staging_area/quack'
 shared_examples_for 'local_repository#staging_area' do
   it {should respond_to :staging_area}
   its(:staging_area) {should be}
-  it_should_behave_like 'staging_area#quack', described_class.new.staging_area
+  its(:staging_area) {should quack_like AmpRepoSpec::AStagingArea}
 end
--- lib/amp_repo_spec/staging_area/quack.rb
+++ lib/amp_repo_spec/staging_area/quack.rb
@@ -1,27 +1,32 @@
-shared_examples_for 'staging_area#quack' do |duck|
-  [
-    :tracking?,
-    :examine_named_files,
-    :find_with_patterns,
-    :walk,
-    :status,
-    :add,
-    :remove,
-    :normal,
-    :forget,
-    :copy,
-    :move,
-    :include,
-    :exclude,
-    :file_status,
-    :vcs_dir,
-    :save,
-    :all_files,
-    :ignoring_directory?,
-    :ignoring_file?,
-    :file_precise_status,
-    :calculate_delta,
-  ].each do |meth|
-    it {(duck || subject).should respond_to(meth)}
+AmpRepoSpec::AStagingArea = [
+  :tracking?,
+  :examine_named_files,
+  :find_with_patterns,
+  :walk,
+  :status,
+  :add,
+  :remove,
+  :normal,
+  :forget,
+  :copy,
+  :move,
+  :include,
+  :exclude,
+  :file_status,
+  :vcs_dir,
+  :save,
+  :all_files,
+  :ignoring_directory?,
+  :ignoring_file?,
+  :file_precise_status,
+  :calculate_delta,
+]
+class <<AmpRepoSpec::AStagingArea
+  def inspect
+    'a staging area'
   end
+end
+
+shared_examples_for "staging_area#quack" do
+  it {should quack_like AmpRepoSpec::AStagingArea}
 end
