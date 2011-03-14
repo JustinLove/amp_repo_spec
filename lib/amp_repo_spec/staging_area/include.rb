88ebefeed05bfc041ffb3f0575a695e15315ebdb
Justin Love <git@JustinLove.name>
2011-03-14 12:59:16 -0500

    staging_area include

---
+++ lib/amp_repo_spec/staging_area/include.rb
@@ -0,0 +1,17 @@
+shared_examples_for 'staging_area#include' do
+  it {should respond_to :include}
+
+  describe 'include', :focus => true do
+    in_a_new_repo do
+      add 'included.file'
+      add 'normal.file'
+      commit
+      modify 'included.file'
+      modify 'normal.file'
+      repo.staging_area.include 'included.file'
+    end
+    subject {repo.staging_area}
+    it {subject.file_status('included.file').should == :modified}
+    it {subject.file_status('normal.file').should == :normal}
+  end
+end
