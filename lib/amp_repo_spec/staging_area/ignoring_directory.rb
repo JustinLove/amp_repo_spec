c00f679eec162ef4321a47261cadc5223f5f545c
Justin Love <git@JustinLove.name>
2011-03-14 13:41:36 -0500

    stub out tests for staging_area pending methods

---
+++ lib/amp_repo_spec/staging_area/ignoring_directory.rb
@@ -0,0 +1,4 @@
+shared_examples_for 'staging_area#ignoring_directory' do
+  it {should respond_to :ignoring_directory?}
+  pending "not tackling ignore yet"
+end
---
+++ lib/amp_repo_spec/staging_area/ignoring_file.rb
@@ -0,0 +1,4 @@
+shared_examples_for 'staging_area#ignoring_file' do
+  it {should respond_to :ignoring_file?}
+  pending "not tackling ignore yet"
+end
